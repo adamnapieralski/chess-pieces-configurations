5891536cd1356ff7f21c451581373906e81d861e
adamnapieralski <adam.napieralski@gmail.com>
2019-03-25 12:41:16 +0100

    Chess piece position

--- chess.h
+++ chess.h
@@ -8,8 +8,21 @@
 #ifndef CHESS_H
 #define CHESS_H
 
-class ChessPiece{
+#include <vector>
 
+
+class Position{
+public:
+    int x;
+    int y;
+};
+
+class ChessPiece{
+public:
+    Position position;
+    std::vector<Position> captureSquares;
+
+    virtual void capture() = 0;
 };
 
 #endif //CHESS_H
