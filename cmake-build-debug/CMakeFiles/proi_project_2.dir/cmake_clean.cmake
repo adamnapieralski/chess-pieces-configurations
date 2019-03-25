8498a2768e4cf1667936ce807bdb83ff8ec639b2
adamnapieralski <adam.napieralski@gmail.com>
2019-03-25 11:52:03 +0100

    Project initialization

---
+++ cmake-build-debug/CMakeFiles/proi_project_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proi_project_2.dir/main.cpp.o"
+  "proi_project_2.pdb"
+  "proi_project_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proi_project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
